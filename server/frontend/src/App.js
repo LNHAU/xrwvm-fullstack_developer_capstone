72793eb3f28f46cb3b3264725b519b5cc52c89f6
HELENE-AUERBACH <ln_au@yahoo.fr>
2026-07-30 10:17:51 -0400

    Add 'User Management'

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,9 +1,11 @@
 import LoginPanel from "./components/Login/Login"
+import RegisterPanel from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
+      <Route path="/register" element={<RegisterPanel />} />
       <Route path="/login" element={<LoginPanel />} />
     </Routes>
   );
