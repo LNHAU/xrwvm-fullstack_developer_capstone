343760b41f0c3ab40095ee1c96eed7fcc6fec119
HELENE-AUERBACH <ln_au@yahoo.fr>
2026-07-31 13:46:03 -0400

    Corrections for linted python files

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -10,25 +10,27 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
+
 def get_request(endpoint, **kwargs):
-# Add code for get requests to back end
+    # Add code for get requests to back end
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
     request_url = backend_url+endpoint+"?"+params
     print("GET from {} ".format(request_url))
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception as err:
         # If any error occurs
         print("Network exception occurred")
 
+
 def analyze_review_sentiments(text):
-# Consume the microservice to analyze sentiments
-# Add code for retrieving sentiments
+    # Consume the microservice to analyze sentiments
+    # Add code for retrieving sentiments
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
         # Call get method of requests library with URL and parameters
@@ -38,13 +40,15 @@ def analyze_review_sentiments(text):
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
+
 def post_review(data_dict):
-# Add code for posting review
-# The dictionary would take all the values required for the dealership review as key-value, pair.
+    # Add code for posting review
+    # The dictionary would take all the values required for the dealership review
+    # as key-value, pair.
     request_url = backend_url+"/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
-        print("Network exception occurred")
+    except Exception as err:
+    print("Network exception occurred")
